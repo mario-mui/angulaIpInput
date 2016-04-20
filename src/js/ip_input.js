fcc07011ad4433b2b9b9a9279b1f755d42f5bca9
mario ma <mario.ma@trantect.com>
2016-04-20 18:03:15 +0800

    first commit

---
+++ src/js/ip_input.js
@@ -0,0 +1,151 @@
+angular.module('ipv4-input-directive', []).directive('gkIpv4Input', function() {
+    var backup = function(value) {
+        var input = this;
+        if (index !== undefined) {
+            input.prevValues = value;
+        } else {
+            throw new Error('backup error');
+        }
+    };
+
+    var revert = function(cells, prevValue) {
+        prevValue.split('.').forEach(function(value, index) {
+            if (index > 4) throw new Error('index more than 4');
+            angular.element(cells[index]).val(value);
+        });
+    };
+
+    var selectCell = function(index) {
+        var input = this;
+        if (index === undefined || index < 0 || index > 3) return;
+        input.find("input")[index].select();
+    };
+
+    var isValidIPStr = function(ipString) {
+        if (typeof ipString !== 'string') return false;
+
+        var ipStrArray = ipString.split('.');
+        if (ipStrArray.length !== 4) return false;
+
+        return ipStrArray.reduce(function(prev, cur) {
+            if (prev === false || cur.length === 0) return false;
+            return (Number(cur) >= 0 && Number(cur) <= 255) ? true : false;
+        }, true);
+    };
+
+    var getCurIPStr = function(cells) {
+        var str = "";
+        for(var i = 0; i < 4; ++i) {
+            value = angular.element(cells[i]).val();
+            str += (i === 0) ? value : "." + value;
+        }
+        return str;
+    };
+
+    // function for text input cell
+    var getCursorPosition = function() {
+        var cell = this;
+        if ('selectionStart' in cell) {
+            // Standard-compliant browsers
+            return cell.selectionStart;
+        } else if (document.selection) {
+            // IE
+            cell.focus();
+            var sel = document.selection.createRange();
+            var selLen = document.selection.createRange().text.length;
+            sel.moveStart('character', -cell.value.length);
+            return sel.text.length - selLen;
+        }
+        throw new Error("cell is not an input");
+    };
+
+    // link function
+    function link(scope, element, attrs) {
+        var input = angular.element(element.find('div'));
+        var cells = element.find('input');
+        var prevValue = '';
+
+        if (attrs.rwd !== undefined)
+            input.toggleClass('rwd', true);
+
+        scope.$watch(attrs.value, function(value) {
+            scope[attrs.valid] = isValidIPStr(value); // set valid bool
+
+            if (!isValidIPStr(value)) return;
+
+            // set ip address
+            value.split('.').forEach(function(str, index) {
+                angular.element(cells[index]).val(str);
+            });
+        });
+
+        cells.on('focus', function(event) {
+            event.target.select();
+            element.find('div').toggleClass('selected', true);
+        });
+
+        cells.on('focusout', function(event) {
+            element.find('div').toggleClass('selected', false);
+        });
+
+        Array.prototype.forEach.call(cells, function(cell, index) {
+            angular.element(cell).on('keydown', function(e) {
+                if(e.keyCode >= 48 && e.keyCode <= 57 || e.keyCode >= 96 && e.keyCode <= 105) { // numbers
+                    prevValue = scope[attrs.value];
+                } else if(e.keyCode == 37 || e.keyCode == 39) { //left key. right key
+                    if (e.keyCode == 37 && getCursorPosition.call(cell) === 0) {
+                        selectCell.call(input, index - 1);
+                        e.preventDefault();
+                    }
+                    else if (e.keyCode == 39 && getCursorPosition.call(cell) === angular.element(cell).val().length) {
+                        selectCell.call(input, index + 1);
+                        e.preventDefault();
+                    }
+                } else if(e.keyCode == 9) { // allow tab
+                } else if(e.keyCode == 8 || e.keyCode == 46) { // allow backspace, delete
+                } else {
+                    e.preventDefault();
+                }
+            });
+
+            angular.element(cell).on('keyup', function(e) {
+                // numbers
+                if (e.keyCode >= 48 && e.keyCode <= 57 || e.keyCode >= 96 && e.keyCode <= 105) {
+                    var val = angular.element(this).val();
+                    var num = Number(val);
+
+                    if (num > 255) {
+                        revert(cells, prevValue);
+                    } else if (val.length > 1 && val[0] === "0") {
+                        revert(cells, prevValue);
+                    } else if (val.length === 3) {
+                        scope[attrs.value] = getCurIPStr(cells);
+                        scope.$apply();
+                        selectCell.call(input, index + 1)
+                    } else {
+                        scope[attrs.value] = getCurIPStr(cells);
+                        scope.$apply();
+                    }
+                } else if (e.keyCode == 8 || e.keyCode == 46) {
+                    scope[attrs.value] = getCurIPStr(cells);
+                    scope.$apply();
+                }
+            });
+        });
+    }
+
+    return {
+        restrict: 'E',
+        template:
+        '<div class="ipv4-input">' +
+        '<input type="text" class="ipv4-cell" />' +
+        '<label class="ipv4-dot">.</label>' +
+        '<input type="text" class="ipv4-cell" />' +
+        '<label class="ipv4-dot">.</label>' +
+        '<input type="text" class="ipv4-cell" />' +
+        '<label class="ipv4-dot">.</label>' +
+        '<input type="text" class="ipv4-cell" />' +
+        '</div>',
+        link: link
+    };
+});
